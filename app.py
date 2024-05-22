4f163676d924db354af3291897b7b7df31bd4c8b
Joel Daniel Rico <joeldanielrico@gmail.com>
2024-05-22 12:09:53 -0700

    Handle GET/POST experience requests

--- app.py
+++ app.py
@@ -44,11 +44,32 @@ def experience():
     '''
     Handle experience requests
     '''
-    if request.method == 'GET':
-        return jsonify()
 
-    if request.method == 'POST':
-        return jsonify({})
+    if request.method == "GET":
+        experiences: list[Experience] = data.get("experience", [])
+        if index := request.args.get("index") is not None:
+            try:
+                return jsonify(experiences[index])
+            except ValueError:
+                return jsonify({"error": "Invalid experience index"})
+        else:
+            return jsonify(experiences)
+
+    if request.method == "POST":
+        post_data: dict[str, str] = request.get_json()
+        experiences = data.get("experience", [])
+        experiences.append(
+            Experience(
+                post_data["title"],
+                post_data["company"],
+                post_data["start_date"],
+                post_data["end_date"],
+                post_data["description"],
+                post_data["logo"],
+            )
+        )
+
+        return jsonify({"id": len(data.get("experience", [])) - 1})
 
     return jsonify({})
 
